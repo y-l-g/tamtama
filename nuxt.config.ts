ce6bc431f4b8bf353617780ccd2993cdfe195716
y-l-g <youenn.legouedec@gmail.com>
2025-12-29 11:41:23 +0100

    wip

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/ui',
+    'nuxt-studio',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+
+    studio: {
+    route: '/_studio',
+        repository: {
+      provider: 'github',
+      owner: 'y-l-g',
+      repo: 'tamtama',
+      branch: 'main',
+    }
+  }
+
+})
